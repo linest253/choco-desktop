cd73feb8fcee5328c5db35016422a0a8b3632ff2
Tim Lines <linest253@yahoo.com>
2018-03-22 18:14:25 -0700

    Add generated cookbook content

---
+++ README.md
@@ -0,0 +1,4 @@
+# choco-desktop
+
+TODO: Enter the cookbook description here.
+
---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: choco-desktop
+# Recipe:: default
+#
+# Copyright:: 2018, The Authors, All Rights Reserved.
