ec28830147859524c9588ad9ed57a8c3e476f91e
Tim Lines <linest253@yahoo.com>
2018-03-23 16:22:52 -0700

    Changed to an array/for loop

--- recipes/default.rb
+++ recipes/default.rb
@@ -7,185 +7,21 @@
 chocolatey_package 'chocolatey' do
 #    action :install
     action :upgrade
+    ignore_failure true
 end
 
-chocolatey_package 'googlechrome' do
-    action :upgrade
-end
-
-chocolatey_package 'adobeshockwaveplayer' do
-    action :upgrade
-end
-
-chocolatey_package 'firefox' do
-    action :upgrade
-end
-
-chocolatey_package 'flashplayerplugin' do
-    action :upgrade
-end
-
-chocolatey_package 'adobereader' do
-    action :upgrade
-end
-
-chocolatey_package 'jre8' do
-    action :upgrade
-end
-
-chocolatey_package 'adobeair' do
-    action :upgrade
-end
-
-chocolatey_package '7zip' do
-    action :upgrade
-end
-
-chocolatey_package 'vlc' do
-    action :upgrade
-end
-
-chocolatey_package 'putty.install' do
-    action :upgrade
-end
-
-chocolatey_package 'powershell' do
-    action :upgrade
-end
-
-chocolatey_package 'silverlight' do
-    action :upgrade
-end
-
-chocolatey_package 'flashplayerppapi' do
-    action :upgrade
-end
-
-chocolatey_package 'winscp' do
-    action :upgrade
-end
-
-chocolatey_package 'keepass' do
-    action :upgrade
-end
-
-chocolatey_package 'wunderlist' do
-    action :upgrade
-end
-
-chocolatey_package 'slack' do
-    action :upgrade
-end
-
-chocolatey_package 'adblockpluschrome' do
-    action :upgrade
-end
-
-chocolatey_package 'adblockplus-firefox' do
-    action :upgrade
-end
-
-chocolatey_package 'dropbox' do
+chocolatey_package 'gow' do
     action :upgrade
     ignore_failure true
 end
 
-chocolatey_package 'onedrive' do
-    action :upgrade
-    ignore_failure true
-end
+package_array = ['googlechrome', 'adobeshockwaveplayer', 'firefox', 'flashplayerplugin', 'adobereader', 'jre8', 'adobeair', '7zip', 'vlc', 'putty.install', 'powershell', 'silverlight', 'flashplayerppapi', 'winscp', 'keepass', 'wunderlist', 'slack', 'adblockpluschrome', 'adblockplus-firefox', 'dropbox', 'onedrive', 'googledrive', 'k-litecodecpackfull', 'skype', 'jre8', 'greenshot', 'openoffice', 'classic-shell', 'mobaxterm', 'qbittorrent', 'neovim', 'speccy', 'cpu-z', 'gpu-z', 'treesizefree', 'wiztree', 'rufus', 'yumi', 'lili', 'handbrake', 'hpusbdisk']
 
-chocolatey_package 'googledrive' do
-    action :upgrade
-end
-
-chocolatey_package 'k-litecodecpackfull' do
-    action :upgrade
-end
-
-chocolatey_package 'skype' do
-    action :upgrade
-    ignore_failure true
-end
-
-chocolatey_package 'jre8' do
-    action :upgrade
-end
-
-chocolatey_package 'greenshot' do
-    action :upgrade
-end
-
-chocolatey_package 'openoffice' do
-    action :upgrade
-end
-
-chocolatey_package 'classic-shell' do
-    action :upgrade
-    ignore_failure true
-end
-
-chocolatey_package 'mobaxterm' do
-    action :upgrade
-    ignore_failure true
-end
-
-chocolatey_package 'qbittorrent' do
-    action :upgrade
-end
-
-chocolatey_package 'neovim' do
-    action :upgrade
-end
-
-chocolatey_package 'speccy' do
-    action :upgrade
-    ignore_failure true
-end
-
-chocolatey_package 'cpu-z' do
-    action :upgrade
-    ignore_failure true
-end
-
-chocolatey_package 'gpu-z' do
-    action :upgrade
-    ignore_failure true
-end
-
-chocolatey_package 'treesizefree' do
-    action :upgrade
-    ignore_failure true
-end
-
-chocolatey_package 'wiztree' do
-    action :upgrade
-    ignore_failure true
-end
-
-chocolatey_package 'rufus' do
-    action :upgrade
-    ignore_failure true
-end
-
-chocolatey_package 'yumi' do
-    action :upgrade
-    ignore_failure true
-end
-
-chocolatey_package 'lili' do
-    action :upgrade
-    ignore_failure true
-end
-
-chocolatey_package 'handbrake' do
-    action :upgrade
-    ignore_failure true
-end
-
-chocolatey_package 'hpusbdisk' do
-    action :upgrade
-    ignore_failure true
+package_array.each do |this_package|
+    chocolatey_package this_package do
+        action :upgrade
+        ignore_failure true
+    end
 end
 
 
