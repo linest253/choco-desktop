4a9e5c5b765ef6b9764c87fa30c8f07d784ae071
Tim Lines <linest253@yahoo.com>
2018-04-11 14:37:22 -0700

    More programs added

--- recipes/default.rb
+++ recipes/default.rb
@@ -15,7 +15,7 @@ chocolatey_package 'gow' do
     ignore_failure true
 end
 
-package_array = ['googlechrome', 'adobeshockwaveplayer', 'firefox', 'flashplayerplugin', 'adobereader', 'jre8', 'adobeair', '7zip', 'vlc', 'putty.install', 'powershell', 'silverlight', 'flashplayerppapi', 'winscp', 'keepass', 'wunderlist', 'slack', 'adblockpluschrome', 'adblockplus-firefox', 'dropbox', 'onedrive', 'googledrive', 'k-litecodecpackfull', 'skype', 'jre8', 'greenshot', 'openoffice', 'classic-shell', 'mobaxterm', 'qbittorrent', 'neovim', 'speccy', 'cpu-z', 'gpu-z', 'treesizefree', 'wiztree', 'rufus', 'yumi', 'lili', 'handbrake', 'hpusbdisk']
+package_array = ['googlechrome', 'adobeshockwaveplayer', 'firefox', 'flashplayerplugin', 'adobereader', 'jre8', 'adobeair', '7zip', 'vlc', 'putty.install', 'powershell', 'silverlight', 'flashplayerppapi', 'winscp', 'keepass', 'wunderlist', 'slack', 'adblockpluschrome', 'adblockplus-firefox', 'dropbox', 'onedrive', 'googledrive', 'k-litecodecpackfull', 'skype', 'jre9', 'greenshot', 'openoffice', 'classic-shell', 'mobaxterm', 'qbittorrent', 'neovim', 'speccy', 'cpu-z', 'gpu-z', 'treesizefree', 'wiztree', 'rufus', 'yumi', 'lili', 'handbrake', 'hpusbdisk', 'poweriso', 'sdformatter', 'formatfactory']
 
 package_array.each do |this_package|
     chocolatey_package this_package do
